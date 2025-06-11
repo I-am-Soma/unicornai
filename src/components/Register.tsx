fb10580f1f48741bf88909a33dfc130c04f560fd
I-am-Soma <javirambar@gmail.com>
2025-06-10 20:35:07 -0700

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import supabase from '../supabaseClient';
+import { supabase } from '../utils/supabaseClient';
 
 const Register: React.FC = () => {
   const [email, setEmail] = useState('');
